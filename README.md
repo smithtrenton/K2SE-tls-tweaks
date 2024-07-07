de212e12835f45ceb11a7a09ab1d1cce73397311
Trenton Smith <tlsdude@gmail.com>
2024-07-06 18:22:28 -0600

    Add biolab and greenhouse recipes to advanced chemical plant

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # K2SE-tls-tweaks
 Factorio Mod: Minor tweaks to K2SE for QoL/UPS reasons
+
+## Features
+- Space pipe underground length matches normal pipes
+- Space belt underground length matches express belt undergrounds
+- Deep space belt underground length matches superior belt undergrounds
+- Advanced Assembler:
+  - Update fluid inputs and outputs to 2 sides with 2 inputs and 2 sides with 2 outputs
+  - Additional recipes:
+    - Core fragment processing
+    - Puvlerizer
+    - Crusher
+- Advanced Chemical Plant:
+  - Additional recipes:
+    - Fuel refinery
+    - Oil refinery
+    - Electrolysis plant
+    - Filtration plant
+    - Greenhouse
+    - Biolab
+    - Oil processing(from Biomechanical facility, does not include most space recipes)
+- Advanced Furnace:
+  - Additional recipes:
+    - Casting machine
--- data-updates.lua
+++ data-updates.lua
@@ -1,50 +1,8 @@
-local advanced_assembler = data.raw["assembling-machine"]["kr-advanced-assembling-machine"]
-local advanced_chemical_plant = data.raw["assembling-machine"]["kr-advanced-chemical-plant"]
-local advanced_furnace = data.raw["assembling-machine"]["kr-advanced-furnace"]
-local vanilla_pipe_to_ground = data.raw["pipe-to-ground"]["pipe-to-ground"]
-local space_pipe_to_ground = data.raw["pipe-to-ground"]["se-space-pipe-to-ground"]
-local underground_belt_group = data.raw["underground-belt"]
-local express_belt_underground = underground_belt_group["express-underground-belt"]
-local space_belt_underground = underground_belt_group["se-space-underground-belt"]
-local superior_belt_underground = underground_belt_group["kr-superior-underground-belt"]
-local deep_space_underground_belt_prefix = "se-deep-space-underground-belt-"
-local deep_space_belt_colors = {"black", "blue", "cyan", "green", "magenta", "red", "white", "yellow"}
-
 -- Add more recipes to K2 advanced assembler
+local advanced_assembler = data.raw["assembling-machine"]["kr-advanced-assembling-machine"]
 table.insert(advanced_assembler.crafting_categories, "crushing")
-table.insert(advanced_assembler.crafting_categories, "crafting-or-electromagnetics")
 table.insert(advanced_assembler.crafting_categories, "pulverising")
 table.insert(advanced_assembler.crafting_categories, "core-fragment-processing")
-
--- Add more recipes to K2 advanced chemical plant
-table.insert(advanced_chemical_plant.crafting_categories, "fuel-refinery")
-table.insert(advanced_chemical_plant.crafting_categories, "atmosphere-condensation")
-table.insert(advanced_chemical_plant.crafting_categories, "electrolysis")
-table.insert(advanced_chemical_plant.crafting_categories, "fluid-filtration")
-table.insert(advanced_chemical_plant.crafting_categories, "oil-processing")
-table.insert(advanced_chemical_plant.crafting_categories, "melting")
-table.insert(advanced_chemical_plant.crafting_categories, "fuel-refining")
-table.insert(advanced_chemical_plant.crafting_categories, "vita-growth")
-
--- Add more recipes to K2 advanced furnace
-table.insert(advanced_furnace.crafting_categories, 'casting')
-
--- Update space pipe underground length to match vanilla pipe
-local vanilla_distance = vanilla_pipe_to_ground.fluid_box.pipe_connections[2].max_underground_distance
-space_pipe_to_ground.fluid_box.pipe_connections[2].max_underground_distance = vanilla_distance
-
--- Update space belt underground length to match express belt
-local express_distance = express_belt_underground.max_distance
-space_belt_underground.max_distance = express_distance
-
--- Update deep space belt underground length to match superior belt
-local superior_distance = superior_belt_underground.max_distance
-for _, color in pairs(deep_space_belt_colors) do
-    local deep_space_belt_underground = underground_belt_group[deep_space_underground_belt_prefix .. color]
-    if deep_space_belt_underground then
-        deep_space_belt_underground.max_distance = superior_distance
-    end
-end
 
 -- Update K2 advanced assembler to have more fluid connections to enable normal core fragment processing
 local original_input = advanced_assembler.fluid_boxes[1]
@@ -75,3 +33,44 @@ for _, position in pairs(output_positions) do
 end
 new_fluid_boxes["off_when_no_fluid_recipe"] = true
 advanced_assembler.fluid_boxes = new_fluid_boxes
+
+-- Add more recipes to K2 advanced chemical plant
+local advanced_chemical_plant = data.raw["assembling-machine"]["kr-advanced-chemical-plant"]
+table.insert(advanced_chemical_plant.crafting_categories, "fuel-refinery")
+table.insert(advanced_chemical_plant.crafting_categories, "atmosphere-condensation")
+table.insert(advanced_chemical_plant.crafting_categories, "electrolysis")
+table.insert(advanced_chemical_plant.crafting_categories, "fluid-filtration")
+table.insert(advanced_chemical_plant.crafting_categories, "oil-processing")
+table.insert(advanced_chemical_plant.crafting_categories, "fuel-refining")
+table.insert(advanced_chemical_plant.crafting_categories, "vita-growth")
+table.insert(advanced_chemical_plant.crafting_categories, "growing")
+table.insert(advanced_chemical_plant.crafting_categories, "bioprocessing")
+
+-- Add more recipes to K2 advanced furnace
+local advanced_furnace = data.raw["assembling-machine"]["kr-advanced-furnace"]
+table.insert(advanced_furnace.crafting_categories, 'casting')
+
+-- Update space pipe underground length to match vanilla pipe
+local vanilla_pipe_to_ground = data.raw["pipe-to-ground"]["pipe-to-ground"]
+local space_pipe_to_ground = data.raw["pipe-to-ground"]["se-space-pipe-to-ground"]
+local vanilla_distance = vanilla_pipe_to_ground.fluid_box.pipe_connections[2].max_underground_distance
+space_pipe_to_ground.fluid_box.pipe_connections[2].max_underground_distance = vanilla_distance
+
+-- Update space belt underground length to match express belt
+local underground_belt_group = data.raw["underground-belt"]
+local express_belt_underground = underground_belt_group["express-underground-belt"]
+local space_belt_underground = underground_belt_group["se-space-underground-belt"]
+local express_distance = express_belt_underground.max_distance
+space_belt_underground.max_distance = express_distance
+
+-- Update deep space belt underground length to match superior belt
+local superior_belt_underground = underground_belt_group["kr-superior-underground-belt"]
+local deep_space_underground_belt_prefix = "se-deep-space-underground-belt-"
+local deep_space_belt_colors = {"black", "blue", "cyan", "green", "magenta", "red", "white", "yellow"}
+local superior_distance = superior_belt_underground.max_distance
+for _, color in pairs(deep_space_belt_colors) do
+    local deep_space_belt_underground = underground_belt_group[deep_space_underground_belt_prefix .. color]
+    if deep_space_belt_underground then
+        deep_space_belt_underground.max_distance = superior_distance
+    end
+end
